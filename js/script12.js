36ac41203cb79abff8bc7e9c4f0ffc7254af16d6
biancabthomas <60096443+biancabthomas@users.noreply.github.com>
2021-05-27 11:52:52 -0400

    Add files via upload

--- js/script12.js
+++ js/script12.js
@@ -15,7 +15,7 @@ function scroll() {
         $t.removeClass('reveal_pending').addClass('reveal_visible');
       } else {
         $t.addClass('reveal_pending');
-        if (!rafId) requestAnimationFrame(reveal);  
+        if (!rafId) requestAnimationFrame(reveal);
       }
     }
   });
@@ -23,20 +23,19 @@ function scroll() {
 function reveal() {
   rafId = null;
   var now = performance.now();
-  
+
   if (now - lTime > delay) {
     lTime = now;
     var $ts = $('.reveal_pending');
-    $($ts.get(0)).removeClass('reveal_pending').addClass('reveal_visible');  
+    $($ts.get(0)).removeClass('reveal_pending').addClass('reveal_visible');
   }
-  
-  
+
+
   if ($('.reveal_pending').length >= 1) rafId = requestAnimationFrame(reveal);
 }
 
 $(scroll);
-$(window).scroll(scroll);
-$(window).click(function() {
+$(window).scroll(scroll)(function() {
   $('.reveal').removeClass('reveal_visible').removeClass('reveal_pending');
   lTime = performance.now() + 500;
   var top = $(window).scrollTop();
